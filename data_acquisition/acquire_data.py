83c35220abf5713dcbbf9fd7c5861ac5d4106980
BUBEUS <kubastu11@gmail.com>
2026-05-13 22:14:44 +0200

    sending data

---
+++ data_acquisition/acquire_data.py
@@ -0,0 +1,9 @@
+'''
+Odbiór: Przyjęcie połączenia HTTP/HTTPS.
+
+Normalizacja: Przekształcenie danych do standardu, który rozumie Twoja kolejka.
+
+Publikacja: Wysłanie wiadomości do brokera (np. RabbitMQ/Kafka).
+
+Potwierdzenie: Zwrócenie szybkiej odpowiedzi do nadawcy, by mógł wysłać kolejną paczkę.
+'''
---
+++ data_service/main.py
@@ -0,0 +1,29 @@
+from .send_data import DataSender
+from .iot_generator import DataGenerator
+from .drone_generator import DroneGenerator
+import time
+
+print("Uruchomiono Data Service...")
+
+iot_generator = DataGenerator()
+drone_generator = DroneGenerator()
+data_sender = DataSender(target_url="http://data_acquisition:5000/data")
+
+while True:
+    # Generowanie danych IOT
+    iot_generator.generate_measurement()
+    iot_payload = iot_generator.get_iot_payload()
+
+    # Generowanie danych z drona
+    drone_generator.generate_photo(max_number=5)
+    drone_payload = drone_generator.get_drone_payload()
+
+    # Wysyłanie danych do Data Acquisition
+    print("Wysyłanie danych IOT...")
+    data_sender.send_data(iot_payload)
+
+    print("Wysyłanie danych z drona...")
+    data_sender.send_data(drone_payload)
+
+    # Czekaj 5 sekund przed kolejnym cyklem
+    time.sleep(5)
