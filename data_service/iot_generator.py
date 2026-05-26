bc4e37fb5e8b080ed16c55acd925d202a8f03a38
BUBEUS <kubastu11@gmail.com>
2026-05-26 20:57:24 +0200

    random ids

--- data_service/iot_generator.py
+++ data_service/iot_generator.py
@@ -5,12 +5,14 @@ import random
 class DataGenerator:
     def __init__(self):
         self.client_id = 0
+        self.identifier = 0
 
         self.temperature = 0.0
         self.humidity = 0
         self.pressure = 0
 
     def generate_measurement(self):
+        self.identifier = random.randint(1, 20)
         self.client_id = random.randint(1, 20)
         self.temperature = round(random.uniform(-20.0, 40.0), 2)
         self.humidity = random.randint(0, 100)
@@ -19,6 +21,7 @@ class DataGenerator:
     def get_iot_payload(self):
         return {
             "type": "iot",
+            "identifier": self.identifier,
             "client_id": self.client_id,
             "temperature": self.temperature,
             "humidity": self.humidity,
