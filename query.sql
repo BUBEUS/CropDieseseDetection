1f5833a0ea078393f7ba29fd021e2363fbd6a0c1
BUBEUS <kubastu11@gmail.com>
2026-05-26 21:12:09 +0200

    db query

---
+++ query.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS hot_drone (
+    id INTEGER PRIMARY KEY,
+    client_id INTEGER REFERENCES client(id) ON DELETE CASCADE,
+    identifier INTEGER NOT NULL,
+    photo_number INTEGER NOT NULL,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    altitude REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS hot_iot (
+    id INTEGER PRIMARY KEY,
+    client_id INTEGER REFERENCES client(id) ON DELETE CASCADE,
+    identifier INTEGER NOT NULL,
+    temperature REAL NOT NULL,
+    humidity REAL NOT NULL,
+    pressure REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS client (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS archive_data (
+    id INTEGER PRIMARY KEY,
+    client_id INTEGER REFERENCES client(id) ON DELETE CASCADE,
+    identifier INTEGER NOT NULL,
+    temperature REAL NOT NULL,
+    humidity REAL NOT NULL,
+    pressure REAL NOT NULL,
+    photo_number INTEGER NOT NULL,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    altitude REAL NOT NULL
+);
+
+
+
+
+
