9f66bca9e43bf94e2173e074563eb02e3d83f059
Sersoon <olimpionito007@gmail.com>
2026-05-26 16:26:32 +0200

    test

--- data_acquisition/acquire_data.py
+++ data_acquisition/acquire_data.py
@@ -6,4 +6,5 @@ Normalizacja: Przekształcenie danych do standardu, który rozumie Twoja kolejka
 Publikacja: Wysłanie wiadomości do brokera (np. RabbitMQ/Kafka).
 
 Potwierdzenie: Zwrócenie szybkiej odpowiedzi do nadawcy, by mógł wysłać kolejną paczkę.
+przyklad
 '''
